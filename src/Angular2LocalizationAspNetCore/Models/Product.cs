393b5103c13b8f0c407eb7dad34dc2a0e49bd2f1
damienbod <damien_bod@hotmail.com>
2016-04-27 18:25:25 +0200

    adding a product class for the data

---
+++ src/Angular2LocalizationAspNetCore/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Angular2LocalizationAspNetCore.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+
+        public long Name { get; set; }
+
+        public long Description { get; set; }
+
+        public string FilePath { get; set; }
+    }
+}
