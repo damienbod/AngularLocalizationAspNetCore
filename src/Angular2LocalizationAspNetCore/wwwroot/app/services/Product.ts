11edbf2f1a00bb7ce81db95d70079b7869cb83b0
damienbod <damien_bod@hotmail.com>
2016-04-27 21:36:06 +0200

    adding the angular2 service to get the localized data from the server

---
+++ src/Angular2LocalizationAspNetCore/wwwroot/app/services/Product.ts
@@ -0,0 +1,3 @@
+﻿export class Product {
+    Y: number[];
+}
---
+++ src/Angular2LocalizationAspNetCore/wwwroot/app/services/ProductService.ts
@@ -0,0 +1,39 @@
+﻿import { Injectable } from 'angular2/core';
+import { Http, Response, Headers } from 'angular2/http';
+import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs/Observable';
+import { Configuration } from '../app.constants';
+import { Product } from './Product';
+import { LocaleService } from 'angular2localization/angular2localization';
+
+
+@Injectable()
+export class ProductService {
+
+    private actionUrl: string;
+    private headers: Headers;
+    private isoCode: string;
+
+    constructor(private _http: Http, private _configuration: Configuration, public _locale: LocaleService) {
+        this.actionUrl = `${_configuration.Server}api/Shop/`; 
+        this.isoCode = `${_locale.getCurrentLanguage}-${_locale.getCurrentCountry}`; 
+    }
+
+    private setHeaders() {
+        this.headers = new Headers();
+        this.headers.append('Content-Type', 'application/json');
+        this.headers.append('Accept', 'application/json');
+    }
+
+    // http://localhost:5000/api/Shop/AvailableProducts?culture=de-CH
+    // http://localhost:5000/api/Shop/AvailableProducts?culture=it-CH
+    // http://localhost:5000/api/Shop/AvailableProducts?culture=fr-CH
+    // http://localhost:5000/api/Shop/AvailableProducts?culture=en-US
+    public GetAvailableProducts = (): Observable<Product[]> => {
+        this.setHeaders();
+        return this._http.get(`${this.actionUrl}AvailableProducts?culture=${this.isoCode}`, {
+            headers: this.headers
+        }).map(res => res.json());
+    }
+    
+}
