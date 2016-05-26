acec4ffc4342853b9e17ffec0e1d8157dbbff2ad
damienbod <damien_bod@hotmail.com>
2016-05-26 05:48:16 +0200

    Using Localization.SqlLocalizer from NuGet

--- src/Localization.SqlLocalizer/DbStringLocalizer/SqliteCreateLocalizationRecord.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "LocalizationRecords" (
-    "Id" INTEGER NOT NULL CONSTRAINT "PK_DataEventRecord" PRIMARY KEY AUTOINCREMENT,
-    "Key" TEXT,
-	"ResourceKey" TEXT,
-    "Text" TEXT,
-    "LocalizationCulture" TEXT,
-    "UpdatedTimestamp" TEXT NOT NULL
-)
